da75883b518687490cc444492f6cba3e0b16c916
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2025-10-16 10:15:00 -0700

    Added Connect

--- Week4/2MongoDemo/server.js
+++ Week4/2MongoDemo/server.js
@@ -5,3 +5,12 @@ require("dotenv").config();
 console.log(process.env.CONNECTION_STRING);
 const mongoose = require("mongoose");
 const express = require("express");
+
+//mongoose.connect
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to mongodb"))
+  .catch((err) => console.log(err));
